ab382fdb04a70e2f2ab2ab26c5312c5c3e4e69aa
Evgeniy Sergunin <sergunin.evgeniy@wb.ru>
2023-02-20 13:07:31 +0300

    add l9 t1

--- Урок 8 - Инкапсуляция/task2.py
+++ Урок 8 - Инкапсуляция/task2.py
@@ -7,7 +7,6 @@
 #         return super().__getattribute__(item)
 
 #     def __setattr__(self, name, value):
-#         print()
 #         if value >= 0:
 #             super().__setattr__(name, value)
 #         else:
@@ -19,38 +18,63 @@
 
 class Point:
     def __init__(self):
-        self.__x = 0
-        self.__y = 0
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if value >= 0:
-            self.__x = value
+        self._x = 0
+        self._y = 0
+    def newx(self,newx):
+        if newx < 0:
+            print('Координата должна быть >= 0')
         else:
-            print('Координата должна быть > 0')
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if value >= 0:
-            self.__y = value
+            self._x = newx
+    def newy(self, newy):
+        if newy < 0:
+            print('Координата должна быть >= 0')
         else:
-            print('Координата должна быть > 0')
-
+            self._y = newy
     def __str__(self):
-        return f'Point ({self.__x}, {self.__y})'
+        return f'Point ({self._x}, {self._y})'
+a = input().split()
+b = list(map(int, a))
+ex = Point()
+ex.newx(b[0])
+ex.newy(b[1])
+print(ex)
 
 
 
-x, y = map(int, input().split())
-point = Point()
-point.x = x
-point.y = y
-print(point)
+# class Point:
+#     def __init__(self):
+#         self.__x = 0
+#         self.__y = 0
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, value):
+#         if value >= 0:
+#             self.__x = value
+#         else:
+#             print('Координата должна быть > 0')
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, value):
+#         if value >= 0:
+#             self.__y = value
+#         else:
+#             print('Координата должна быть > 0')
+
+#     def __str__(self):
+#         return f'Point ({self.__x}, {self.__y})'
+
+
+
+# x, y = map(int, input().split())
+# point = Point()
+# point.x = x
+# point.y = y
+# print(point)
---
+++ Урок 9 - Обработка исключений/main.py
@@ -0,0 +1,26 @@
+
+
+def main():
+    # try:
+    #     # print(4/0)
+    #     print(4/2)
+
+    # except Exception as e:
+    #     print(f'error {e}')
+
+    l = ['ahdh', 'asdasdasfdas', 'првыив', 'Привет мир']
+
+    for string in l:
+        print((l))
+    else:
+        print('нашел Прив')
+
+    # finally:
+    #     print('final')
+
+if __name__ == '__main__':
+    main()
+
+
+# s = 'Привет мир'
+# s2 = 'Прив'
